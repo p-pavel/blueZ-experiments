38ae0924a447babda4e0971d3168d984f66f5d0c
Pavel Perikov <perikov@gmail.com>
2023-04-01 18:19:00 +0400

    primitive sasl support

--- src/main/scala/com/perikov/dbus/DBusSASL.scala
+++ src/main/scala/com/perikov/dbus/DBusSASL.scala
@@ -1,3 +1,5 @@
+package com.perikov.dbus
+
 import cats.*
 import cats.implicits.*
 import cats.effect.*
@@ -12,9 +14,11 @@ class DBusSASL[F[_]: Async](using log: Scribe[F]):
   import log.*
 
   import fs2.Chunk
-  def toHex(n: Int): String = n.toString.getBytes.map(_.toInt.toHexString).mkString
+  private def toHex(n: Int): String =
+    n.toString.getBytes.map(_.toInt.toHexString).mkString
+    
   type Error = String
-  def authenticate(sock: Socket[F], uid: Int): F[Option[Error]] = 
+  def authenticate(sock: Socket[F], uid: Int): F[Option[Error]] =
     def sendString(s: String) =
       sock.write(Chunk.array((s + "\r\n").getBytes)) *> trace(
         s"Sent: $s"
@@ -25,22 +29,24 @@ class DBusSASL[F[_]: Async](using log: Scribe[F]):
         .value
         .flatTap(s => trace(s"Received: $s"))
 
-    for //TODO: Check for errors
+    for // TODO: Check for errors
       _ <- trace("Sending 0")
       _ <- sock.write(Chunk(0.toByte))
       _ <- sendCmd("AUTH EXTERNAL " + toHex(uid))
       _ <- sendCmd("NEGOTIATE_UNIX_FD")
       _ <- sendString("BEGIN")
-    yield None 
+    yield None
 
 object SASL extends IOApp.Simple:
   given log: Scribe[IO] = scribe.cats.io
   val address = UnixSocketAddress("/var/run/dbus/system_bus_socket")
   val sockResource = fs2.io.net.unixsocket.UnixSockets[IO].client(address)
 
-  def run: IO[Unit] = sockResource.use { sock => 
-    DBusSASL[IO].authenticate(sock, 1001).flatMap { //TODO: Get uid from somewhere
-      case Some(err) => IO(println(s"Error: $err"))
-      case None => IO(println("Success"))
-    }
+  def run: IO[Unit] = sockResource.use { sock =>
+    DBusSASL[IO]
+      .authenticate(sock, 1001)
+      .flatMap { // TODO: Get uid from somewhere
+        case Some(err) => IO(println(s"Error: $err"))
+        case None      => IO(println("Success"))
+      }
   }
